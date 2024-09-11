0adad038ebe46df17b2d401fdccf645304ce24ef
yan123hao <yan37449@qq.com>
2024-09-11 21:46:24 +0800

    Site updated: 2024-09-11 21:46:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/10/xin/","2024/09/10/hello-world/","2024/09/11/wo-de-bo-ke/","2024/09/11/chebggong/"];function toRandomPost(){
+var posts=["2024/09/11/chebggong/","2024/09/10/hello-world/","2024/09/11/wo-de-bo-ke/","2024/09/10/xin/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
