3580b4017b5d820992af2a3fe61876bf6770f86b
yan123hao <yan37449@qq.com>
2024-09-13 21:58:34 +0800

    Site updated: 2024-09-13 21:58:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/11/chebggong/","2024/09/10/hello-world/","2024/09/11/wo-de-bo-ke/","2024/09/10/xin/","2024/09/12/blog/"];function toRandomPost(){
+var posts=["2024/09/11/chebggong/","2024/09/10/hello-world/","2024/09/11/wo-de-bo-ke/","2024/09/10/xin/","2024/09/12/blog/","2024/09/13/blog-1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
